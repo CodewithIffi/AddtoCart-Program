e0ddcd84f5fb1d44d0148e34955690f1b714564a
unknown <ifficodder@gmail.com>
2024-05-21 04:06:48 +0500

    footer crated

--- cart.js
+++ cart.js
@@ -1,4 +1,4 @@
-console.log('cart items');
+// console.log('cart items');
 
 const cartItems = JSON.parse(localStorage.getItem('cartItems'));
 const div = document.querySelector('div');
@@ -6,20 +6,22 @@ const div = document.querySelector('div');
 
 
 function renderItems() {
-
-
-
+    let total = 0;
     if (cartItems != null && cartItems.length > 0) {
         for (let i = 0; i < cartItems.length; i++) {
+            total += cartItems[i].price * cartItems[i].quantity
             div.innerHTML += `
             <div class="border border-light roundedd m-5 p-3"> 
-            <h1> Brand: ${cartItems[i].brand + ' ' + cartItems[i].model}</h1>
-            <h1>Price: ${cartItems[i].price}</h1>
-            <button onclick="deleteItems()" class="btn btn-danger mt-3">Delete</button>
+            <h1> Brand: ${cartItems[i].brand + ' ' + cartItems[i].model}</h1>Quantity:
+            <button class="btn btn-primary" onclick="addQuantity(${i})"> + </button>
+            <button class="btn btn-primary" onclick="lessQuantity(${i})"> - </button>
+            <h1>Price: ${cartItems[i].price * cartItems[i].quantity}</h1>
+            <button class="btn btn-primary" onclick="deleteItem(${i})">Delete</button>
             </div>
 
             `
         }
+        totalamount.innerHTML = `Total Amount = ${total}`
 
     } else {
         div.innerHTML = `
@@ -29,3 +31,28 @@ function renderItems() {
 }
 
 renderItems()
+
+function addQuantity(i){
+    div.innerHTML = "";
+    cartItems[i].quantity =+ 1
+    renderItems()
+    console.log(cartItems);
+}
+function lessQuantity(i){
+    div.innerHTML = ''
+    if(cartItems[i].quantity <= 1){
+        cartItems.splice(i , 1);
+    }else{
+        cartItems[i].quantity -= 1
+    }
+    renderItems()
+}
+
+function deleteItem(i){
+    div.innerHTML = ''
+    cartItems.splice(i , 1);
+    totalamount.innerHTML = ''
+    renderItems()
+}
+
+
